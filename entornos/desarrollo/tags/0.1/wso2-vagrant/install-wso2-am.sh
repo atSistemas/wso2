071a17359c8150558a4d5364df6975e34587aaf5
osanzs <oscar.sanz@gmail.com>
2016-08-02 15:11:35 +0200

    Se incluye la version 2.0.0 de API Manager

--- entornos/desarrollo/tags/0.1/wso2-vagrant/install-wso2-am.sh
+++ entornos/desarrollo/tags/0.1/wso2-vagrant/install-wso2-am.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #Generacion DOCKER WSO2-AM
 PRODUCT_WSO2="wso2am"
-VERSION_WSO2="1.10.0"
+VERSION_WSO2="2.0.0"
 HOME="/home/vagrant"
 
 cd $HOME/wso2-dockerfiles-1.0.0/common/provision/default/files/
-sudo wget --user-agent="testuser" --referer="http://connect.wso2.com/wso2/getform/reg/new_product_download" http://product-dist.wso2.com/products/api-manager/1.10.0/wso2am-1.10.0.zip
+sudo wget --user-agent="testuser" --referer="http://connect.wso2.com/wso2/getform/reg/new_product_download" http://product-dist.wso2.com/products/api-manager/2.0.0/wso2am-2.0.0.zip
 cd $HOME/wso2-dockerfiles-1.0.0/$PRODUCT_WSO2
 sudo cp $HOME/wso2-suite/dockerfiles/$PRODUCT_WSO2/* $HOME/wso2-dockerfiles-1.0.0/$PRODUCT_WSO2
 sudo cp $HOME/wso2-suite/scripts/$PRODUCT_WSO2/* $HOME/wso2-dockerfiles-1.0.0/$PRODUCT_WSO2
